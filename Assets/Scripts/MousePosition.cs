a68d9633f5a4774e291d59d74b7a757f0ee96ff1
IsisAmaya <icamayaa@eafit.edu.co>
2024-05-20 18:29:06 -0500

    feeback couldown and soundtrack of game

--- Assets/Scripts/MousePosition.cs
+++ Assets/Scripts/MousePosition.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-
 public class MousePosition : MonoBehaviour
 {
     public GameObject player;
@@ -13,56 +12,60 @@ public class MousePosition : MonoBehaviour
     public bool powerup = false;
     public float shootCooldown = 1f; // Tiempo de reutilización en segundos
     private float nextShootTime = 0f;
+    public AudioClip cooldownActiveSound;
+    public AudioClip shootReadySound;
+    private AudioSource audioSource;
 
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
         screenPosition = Input.mousePosition;
-
         Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-
         transform.position = worldPosition;
-
         shoot(ray);
     }
 
     private void shoot(Ray ray)
     {
-        if (Time.time < nextShootTime)
-        {
-            return;
-        }
-
         if (Input.GetMouseButtonDown(0))
         {
-            nextShootTime = Time.time + shootCooldown;
-            Debug.Log(nextShootTime);
-
-
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            if (Time.time >= nextShootTime)
             {
-                Transform objectHit = hit.transform;
-                HitBoxCube hitBoxCube = objectHit.GetComponent<HitBoxCube>();
-                ItemHitBox itemHitBox = objectHit.GetComponent<ItemHitBox>();
-                Player score = player.GetComponent<Player>();
-                if (hitBoxCube != null)
-                {
-                    hitBoxCube.golpe();
-                    score.addScore(1);
-                }
+                nextShootTime = Time.time + shootCooldown;
+                audioSource.PlayOneShot(shootReadySound);
+                Debug.Log(nextShootTime);
 
-                if (itemHitBox != null)
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
                 {
-                    itemHitBox.powerup();
+                    Transform objectHit = hit.transform;
+                    HitBoxCube hitBoxCube = objectHit.GetComponent<HitBoxCube>();
+                    ItemHitBox itemHitBox = objectHit.GetComponent<ItemHitBox>();
+                    Player score = player.GetComponent<Player>();
+                    if (hitBoxCube != null)
+                    {
+                        hitBoxCube.golpe();
+                        score.addScore(1);
+                    }
+
+                    if (itemHitBox != null)
+                    {
+                        itemHitBox.powerup();
+                    }
                 }
+            }
+
+            else {
+                audioSource.PlayOneShot(cooldownActiveSound);
             }
         }
     }
